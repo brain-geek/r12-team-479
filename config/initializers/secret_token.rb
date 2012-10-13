ddd243ac08b681b7bc1c258b8a839c514cda1748
Alex Rozumey <brain-geek@yandex.ua>
2012-10-13 12:59:13 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Codeage::Application.config.secret_token = '7d7cb54254c20391e805f4068217287477aff3483fa798388f6fe42b00b7d704e4655c73a982241c668b51d0610425123792cab9287e8c509b2735f2d4877387'
